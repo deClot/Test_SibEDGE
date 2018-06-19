56033b1942754dc5217c70f3c2df560dd7fea8d5
Nadezda Kashirina <refenement@mail.ru>
2018-06-19 16:53:56 +0700

    Create a new file and write info in it

--- test.py
+++ test.py
@@ -11,7 +11,20 @@ def enter_path():
         else:
             return path_dir
 
+path_dir = enter_path ()
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
+name_file = script_dir + '/result.txt'
+file_res = open (name_file, 'w')
 
+to_write = ''
+files = os.listdir(path_dir)
+for name in files:
+    fullname = os.path.join(path_dir, name)
+    if os.path.isfile(fullname):
+        to_write = to_write + '"' + name + '",'
+file_res.write ('[%s]' % (to_write[:len(to_write)-1]))
         
+
+
+file_res.close()
