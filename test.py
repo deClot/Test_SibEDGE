31291d20750e6e4a0ff6f891566bae34ee27e43c
Nadezda Kashirina <refenement@mail.ru>
2018-06-19 17:07:01 +0700

    Transfer in function work_with_files

--- test.py
+++ test.py
@@ -11,20 +11,23 @@ def enter_path():
         else:
             return path_dir
 
+def work_with_files (path_dir):
+    to_write = ''
+    files = os.listdir(path_dir)
+    for name in files:
+        fullname = os.path.join(path_dir, name)
+        if os.path.isfile(fullname):
+            to_write = to_write + '"' + name + '",'
+    file_res.write ('[%s]' % (to_write[:len(to_write)-1]))
+
+        
 path_dir = enter_path ()
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
-name_file = script_dir + '/result.txt'
-file_res = open (name_file, 'w')
-
-to_write = ''
-files = os.listdir(path_dir)
-for name in files:
-    fullname = os.path.join(path_dir, name)
-    if os.path.isfile(fullname):
-        to_write = to_write + '"' + name + '",'
-file_res.write ('[%s]' % (to_write[:len(to_write)-1]))
-        
+name_file = os.path.join(script_dir,'result.txt')
+#if os.path.exists(name_file):
+    
+file_res = open (name_file, 'w')     
 
 
 file_res.close()
